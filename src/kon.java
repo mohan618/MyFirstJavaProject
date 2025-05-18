8b3407a39557708ea5bd77b03409362acc09898c
mohan618 <mohankumar56aps@gmail.com>
2025-05-18 15:39:04 +0530

    zxc

---
+++ src/kon.java
@@ -0,0 +1,11 @@
+import  java.util.*;
+public class mohan {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        String c=a+b;
+        System.out.println("The Input String is : "+ c);
+    }
+
+}
