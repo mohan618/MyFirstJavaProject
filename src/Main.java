042878207f9298c47ac678b274e812ab3af4067b
mohan618 <mohankumar56aps@gmail.com>
2025-05-18 15:15:17 +0530

    Initial commit foe this project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println("Sum of a and b "+ c);
     }
 }
