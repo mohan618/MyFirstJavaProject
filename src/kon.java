1f09ebe26b79db0d26dcb993951cc5122d68d371
mohan618 <mohankumar56aps@gmail.com>
2025-05-18 15:39:31 +0530

    zxc

--- src/kon.java
+++ src/kon.java
@@ -1,5 +1,5 @@
 import  java.util.*;
-public class mohan {
+public class kon {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String a=sc.nextLine();
